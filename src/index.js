a37efafa20c89a1149431bddd6fa1198af942b7e
evgenijkorob <evgenij.k.wie@gmail.com>
2018-09-13 10:14:44 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+const OPENING = 0, CLOSING = 1;
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+  if (str.length < 2)
+    return false;
+  for (var i = 0, brackets = [], currBracketParam, lastBracketParam; i < str.length; i++) {
+    currBracketParam = setBracketParam.call(bracketsConfig, str[i]);
+    if (i > 0) {
+      lastBracketParam = setBracketParam.call(bracketsConfig, brackets[brackets.length - 1]);
+      if (isPair.call(bracketsConfig, lastBracketParam, currBracketParam)) {
+        brackets.pop();
+        continue;
+      }
+    }
+    if (currBracketParam.state == OPENING) 
+      brackets.push(str[i]);
+    else 
+      return false;
+  }
+  if (brackets.length > 0)
+    return false;
+  else
+    return true;
+}
+
+function setBracketParam(bracket) {
+  for (var i = 0; i < this.length; i++) {
+    switch(bracket) {
+      case (this[i][OPENING]):
+        return {type: i, state: OPENING};
+      case (this[i][CLOSING]):
+        return {type: i, state: CLOSING};
+    }
+  }
+  return {type: -1, state: undefined};
+}
+
+function isPair(first, second) {
+  if (first.type != second.type)
+    return false;
+  if (this[first.type][OPENING] == this[second.type][CLOSING])
+    return true;
+  return first.state == OPENING && second.state == CLOSING;
 }
